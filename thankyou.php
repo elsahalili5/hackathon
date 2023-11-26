96cba0379bd4269e60b11dc868f67a254d143022
elsahalili5 <elsahalilii5@gmail.com>
2023-11-26 02:11:39 +0100

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -51,8 +51,9 @@
 
     <div class="container">
         <h1>Thank You for Your Donation!</h1>
-        <p>Your support means a lot to us. With your generous contribution, we can continue making a difference.</p>
-
+        <p>Thanks to your contribution, we are one step closer to achieving our mission and making a positive difference .</p>
+        <p>With gratitude,<br>ECO FUND</p>
+        <p> If you have any questions or would like to learn more about how your donation is making an impact, please do not hesitate to <a href="mailto:info@example.com">contact us</a>.
         <p><a href="index.php">Back to Home</a></p>
     </div>
 
